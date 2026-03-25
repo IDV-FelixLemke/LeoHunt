975f5ad7b8b93fc4bbacb41ffb15bf18ddba7c41
Felix Lemke <fl@informatik-dv.com>
2026-03-25 21:51:35 +0100

    load step data, check answer

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+const steps = [{
+    imgsrc: "imgHints/HohenmStadtkirche.jpg",
+    question: "Aus welchem Jahr stammt die Glocke neben der Kirche?",
+    answers: [
+        "1805",
+        "1903",
+        "die Zukunft",
+        "2011"
+    ],
+    correctAnswer: "2"
+}];
+var curStep = undefined;
+
 function init() {
+    curStep = steps[0]; // for now only one step
+    $("#imageHint img").attr("src", curStep.imgsrc);
+    $("#Question").text(curStep.question);
+    let answers = '';
+    curStep.answers.forEach((ans,idx) => {
+        answers += `
+            <li data-answer="${idx+1}">${ans}</li>
+        `;
+    });
+    $("#Answers").html(answers);
+    
     $("#Answers li").each((idx, elem) => {
         let answer = $(elem).data("answer");
         $(elem).on("click", () => {
@@ -11,7 +35,13 @@ function init() {
 function checkAnswer() {
     let answer = $("#Answers .selected").data("answer");
     if(!answer) return;
-    console.log("Is answer", answer, "correct?");
+    if(curStep.correctAnswer == answer) {
+        $("#Answers .selected").addClass("correct");
+        console.log("Answer is correct");
+    } else {
+        console.log("Wrong Answer");
+        $("#Answers .selected").addClass("wrong");
+    }
 }
 
 init();
